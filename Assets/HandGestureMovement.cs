fbe093d1c935576651ef71cd99ae634ffaede13c
rijumas <mauricio3parker@gmail.com>
2025-04-08 13:15:51 -0500

    hachaMenuHand

--- Assets/HandGestureMovement.cs
+++ Assets/HandGestureMovement.cs
@@ -2,36 +2,25 @@ using System.Dynamic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class HandGestureMovement : MonoBehaviour
-{
+public class HandGestureMovement : MonoBehaviour{
     public Transform xrCamera;
     public float speed = 2.0f;
     private Rigidbody rb;
     private bool isMoving = false;
-
-    void Start()
-    {
+    void Start(){
         rb = GetComponent<Rigidbody>();
     }
-
-    void Update()
-    {
+    void Update(){
         if (isMoving && xrCamera != null)
         {
             Vector3 forwardDirection = new Vector3(xrCamera.forward.x, 0, xrCamera.forward.z).normalized;
             rb.linearVelocity = forwardDirection * speed;
         }
     }
-
-    public void OnRightFistBumpDetected()
-    {
-        // Set the flag to start continuous movement
+    public void OnRightFistBumpDetected(){
         isMoving = true;
     }
-
-    public void StopMovement()
-    {
-        // Set the flag to stop movement and zero out velocity
+    public void StopMovement(){
         isMoving = false;
         rb.linearVelocity = Vector3.zero;
     }
