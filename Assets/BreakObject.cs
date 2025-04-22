83dd53958bfbad22c584684fa4f5f56b835a64db
Emmanuel <emmanuel.martinez@ucsp.edu.pe>
2025-04-22 13:11:52 -0500

    explosiones

---
+++ Assets/BreakObject.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BreakObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"El breaker tocó a: {other.name}");
+        if (other.gameObject.layer == LayerMask.NameToLayer("Breakable"))
+        {
+            MeshDestroy meshDestroy = other.GetComponentInParent<MeshDestroy>();
+
+            if (meshDestroy != null)
+            {
+                meshDestroy.TryDestroy();
+            }
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
